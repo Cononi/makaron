a68780db872393f6e262348bf8a6c48d4473ee50
hsy <byhoon3@icloud.com>
2021-12-13 16:09:05 +0900

    메퍼 작성중2

--- src/main/java/com/jua/makaron/mapper/MypageMapper.java
+++ src/main/java/com/jua/makaron/mapper/MypageMapper.java
@@ -1,8 +1,13 @@
 package com.jua.makaron.mapper;
 
+import java.util.List;
+
+import com.jua.makaron.vo.CouponVO;
 import com.jua.makaron.vo.CustomerVO;
+import com.jua.makaron.vo.Customer_ratingBenefitVO;
 import com.jua.makaron.vo.Customer_ratingVO;
 import com.jua.makaron.vo.OrderListVO;
+import com.jua.makaron.vo.PointVO;
 import com.jua.makaron.vo.QuestionVO;
 import com.jua.makaron.vo.ReviewVO;
 import com.jua.makaron.vo.ShippingVO;
@@ -10,43 +15,41 @@ import com.jua.makaron.vo.ShippingVO;
 public interface MypageMapper {
 	
 	//0-1. 유저 이름 얻기
-	public CustomerVO getCustomerName(String id);
+	public String getCustomerName(String id);
 	
-	//0-2. 유저 등급 얻기
+	//0-2. 유저 등급/혜택 얻기
 	public Customer_ratingVO getCustomerRating(String id);
+	public Customer_ratingBenefitVO getCustomerRaingBenefit(String id);
 	
-	//0-3. 유저 적립금 잔액 얻기
+	//0-3. 유저 적립금 잔액/내역 얻기
 	public int getPointBalance(String id);
+	public List<PointVO> getPointHistory(String id);
 	
-	//0-4. 유저 쿠폰 개수 얻기
+	//0-4. 유저 쿠폰 개수/내역 얻기
 	public int getCouponQuantity(String id);
+	public List<CouponVO> getCouponHistory(String id);
 	
 	//1. 주문내역 불러오기
-	public OrderListVO getOrderList(String id);
+	public List<OrderListVO> getOrderList(String id);
 	
 	//2-1. 배송지 불러오기
 	public ShippingVO getShipping(String id);
 	
 	//2-2. 배송지 추가하기
-	public void insertShipping(String id);
+	public void insertShipping(String id, String Nickname, String postal_code, String address_base, String address_sub, String address_type);
 	
-	//2-3. 배송지 수정하기
-	public void updateShipping(String id);
-	
-	//2-4. 배송지 삭제하기
-	public void deleteShipping(String id);
+	//2-3. 배송지 삭제하기
+	public void deleteShipping(String no);
 	
 	//3-1. 후기 내역 불러오기
 	public ReviewVO getReviewList(String id);
 	
-	//3-2. 후기 작성하기
-	public void insertReview(String id);
+	//3-2. 후기 작성하기(글/이미지)
+	public void insertReview(String id, String product_title, String review_title, String review_content);
+	public void insertReviewImage(String id, String file_url, String file_name);
 	
-	//3-3. 후기 수정하기
-	public void updateReview(String id);
-	
-	//3-4. 후기 삭제하기
-	public void deleteReview(String id);
+	//3-3. 후기 삭제하기
+	public void deleteReview(String id, String no);
 	
 	//4-1. 상품 문의 내역 불러오기
 	public QuestionVO getQuestionList(String id);
---
+++ src/main/java/com/jua/makaron/vo/Customer_ratingBenefitVO.java
@@ -0,0 +1,8 @@
+package com.jua.makaron.vo;
+
+public class Customer_ratingBenefitVO {
+	private String name;
+	private String rating_name;
+	private int standard;
+	private int accrual_rate;
+}
